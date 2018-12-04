075b93ff0d2425e1f86cc94dac5fe1e23c9b66a9
Haashim Rehan <haashimr@hotmail.com>
2018-12-03 21:28:10 -0500

    Tuned Camera turning

--- AutonomousRobot/AutonomousRobot.ino
+++ AutonomousRobot/AutonomousRobot.ino
@@ -180,6 +180,6 @@ void loop()
   RR.updateRPM(false);
 
   cam.getSpecialBlocks(GREEN);
-  pointToBlock(cam.blocks[0], 5);
+  pointToBlock(cam.blocks[0], 10);
 
 }
--- AutonomousRobot/Camera.ino
+++ AutonomousRobot/Camera.ino
@@ -1,32 +1,33 @@
 boolean pointToBlock (Block target, int hedge) {//Points to a Block that is sent in
   if (cam.getBlock()) { //Check if there is blocks
-    alignRobot(target, hedge);
+    return alignRobot(target, hedge);
   } else {
     delay(20);
     if (cam.getBlock()) { //check again
-      alignRobot(target, hedge);
+      return alignRobot(target, hedge);
     } else { //There are no blocks
       Serial.println("No Block Detected");
     }
   }
-  return false;
 }
 
-void alignRobot(Block target, int hedge) {
+boolean alignRobot(Block target, int hedge) {
   int width = 320; // pixy cam width
   if (target.x > width / 2 + hedge) {
     //Turns Right until the blocks x val is within a range
     turn(1);
-    Serial.print("RIGHT");
+    Serial.println("RIGHT");
   }
   else if (target.x < width / 2 - hedge) {
     //Turns Left until the blocks x val is within a range
-    Serial.print("LEFT");
+    Serial.println("LEFT");
     turn(-1);
   }
   else if (target.x > width / 2 - hedge && target.x < width / 2 + hedge) {
     //Stops once the block is within a range
-    Serial.print("CENTER");
+    Serial.println("CENTER");
+    drive2(0);
     return true;
   }
+  return false;
 }
